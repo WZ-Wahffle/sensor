c3ea52719dd0f9ef4dfafc657a4487684bc4d963
Valentin Werner <154072360+valwerner2@users.noreply.github.com>
2024-06-23 13:26:24 +0200

    Ich habe das Projekt erweitert (#1)
    * Add files via upload
    
    This is just for testing, ignore
    
    * Add files via upload
    
    those are correct
    
    * Added a class to make it easier to extend the states.
    Every Class contains the path to the base of the bmp files. Bmp files have to be 240x240 and will be sent to the RaspberryPi
    
    * Added a class to make it easier to extend the states.
    Every Class contains the path to the base of the bmp files. Bmp files have to be 240x240 and will be sent to the RaspberryPi
    
    * Delete testBmp directory
    
    * Kotlin Code is now documented
    
    * Kotlin Code is now documented
    
    * C-Code added and README.md updated
    
    ---------
    
    Co-authored-by: Valentin Werner <vwerner1@stud.hs-offenburg.de>

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Ausführen
+# Kotlin-Teil
+## Ausführen
 Abhängigkeiten:
 - kotlin
 - kotlinc
-## Methode 1: IntelliJ
+### Methode 1: IntelliJ
 Das GitHub-Repo kann ohne weiteres in IntelliJ geklont und die Dateien ausgeführt werden. Es sei zu erwähnen, dass evaluateInput von Kommandozeilenargumenten abhängig ist.
 
-## Methode 2: CLI
+### Methode 2: CLI
 Das Kotlin-CLI kann auch ohne weiteres verwendet werden, um die beiden Dateien zu bauen. Dazu muss nur Kotlin an sich installiert sein, welches in allen gängigen Paket-Managern erhältlich ist. Alternativ können Releases auch direkt über [das offizielle Repository](https://github.com/JetBrains/kotlin/releases) erhalten werden.
 
 Sobald heruntergeladen, können die Programme folgendermaßen gebaut und ausgeführt werden (mit Beispieldaten für evaluateInput):
@@ -14,4 +15,18 @@ kotlinc evaluateInput.kt testDataParser.kt
 kotlin TestDataParserKt
 kotlin EvaluateInputKt 2 130 90 100
 ```
-
+# C-Teil
+Für den RaspberryPi 3 Model B+ zur verwendung mit [diesem Display](https://www.waveshare.com/wiki/1.28inch_LCD_Module) geschrieben. Sollte auch auf anderen Modellen Funktionieren. 
+## Ausführen
+Die Anleitung zum Installieren der API ist [hier](https://www.waveshare.com/wiki/1.28inch_LCD_Module) zu finden, die BCM2835-Bibliothek wird verwendet.
+Das Kompilieren erfolg mit der von der API mitgelieferten API und die [main.c](./main.c) ersetzt die gleichnamige Datei in "LCD_Module_RPI_code/RaspberryPi/c/examples" der API.
+Das Ausführen des C-Programms erfolgt mit:
+```
+sudo ./main
+```
+## Erweiterung
+Bei Erweiterung des C-Programms ist darauf zu achten, dass diese Zeile nicht gelöscht wird:
+```
+DEV_GPIO_Mode(18, BCM2835_GPIO_FSEL_OUTP);
+```
+Die API enthält einen Fehler in der Initialisierung des Backlight-Pins, diese Zeile behebt diesen.
--- src/evaluateInput.kt
+++ src/evaluateInput.kt
@@ -2,10 +2,50 @@
  * Mappingprogramm, welches Messwerte als Argumente nimmt und zu Emotion korrespondierende Emoji-UTF8-Codepoints ausgibt.
  * Mit Unix-Philosophie konzipiert, als Bestandteil des Datenstroms gedacht
  * @author Marc Vauderwange, mvauderw@stud.hs-offenburg.de
+ * @author Valentin Werner, vwerner1@stud.hs-offenburg.de
  */
 
 fun main(args: Array<String>)
 {
+    //Intervallen, welche mit dem Analyseprogramm festgestellt wurden
+    //Hier können einfach neue Gemütszustände hinzugefügt werden
+
+    val excited = state(    (1.5..4.0),
+                            (120.0..160.0),
+                            (80.0..100.0),
+                            (90.0..140.0),
+                            6,
+                            "./BitMaps/excited/excited")
+
+    val happy = state(      (1.0..3.5),
+                            (90.0..120.0),
+                            (60.0..80.0),
+                            (50.0..90.0),
+                            8,
+                            "./BitMaps/happy/happy")
+
+    val normal = state(     (0.5..3.0),
+                            (90.0..120.0),
+                            (60.0..80.0),
+                            (60.0..100.0),
+                            8,
+                            "./BitMaps/normal/normal")
+
+    val sad = state(        (0.5..1.5),
+                            (100.0..130.0),
+                            (70.0..90.0),
+                            (70.0..110.0),
+                            8,
+                            "./BitMaps/sad/sad")
+
+    val stressed = state(   (1.0..3.5),
+                            (120.0..160.0),
+                            (80.0..100.0),
+                            (90.0..140.0),
+                            7,
+                            "./BitMaps/stressed/stressed")
+
+    val states = arrayOf(excited, happy, normal, sad, stressed)
     if (args.isEmpty())
     {
         return
@@ -13,51 +53,14 @@ fun main(args: Array<String>)
 
     val (acceleration, bloodPressureSystolic, bloodPressureDiastolic, pulse) = args.map { it.toDouble() }
 
-    // Messwerte werden nach Intervallen geprüft, welche mit dem Analyseprogramm festgestellt wurden
-    if ((1.5..4.0).contains(acceleration) &&
-        (120.0..160.0).contains(bloodPressureSystolic) &&
-        (80.0..100.0).contains(bloodPressureDiastolic) &&
-        (90.0..140.0).contains(pulse))
+    // Aktuelle messwerte werden nach den Intervallen geprüft, welche in den State-Objekten gespeichert sind und dann an den RaspberryPi versendet
+    for(currentState in states)
     {
-        // excited
-        println("\uD83E\uDD29")
+        if(currentState.isState(acceleration, bloodPressureSystolic, bloodPressureDiastolic, pulse))
+        {
+            currentState.sendBmp()
+            break
+        }
     }
 
-    else if ((1.0..3.5).contains(acceleration) &&
-        (90.0..120.0).contains(bloodPressureSystolic) &&
-        (60.0..80.0).contains(bloodPressureDiastolic) &&
-        (50.0..90.0).contains(pulse))
-    {
-        // happy
-        println("\uD83D\uDE00")
-    }
-
-    else if ((0.5..3.0).contains(acceleration) &&
-        (90.0..120.0).contains(bloodPressureSystolic) &&
-        (60.0..80.0).contains(bloodPressureDiastolic) &&
-        (60.0..100.0).contains(pulse))
-    {
-        // normal
-        println("\uD83D\uDE36")
-    }
-
-    else if ((0.5..1.5).contains(acceleration) &&
-        (100.0..130.0).contains(bloodPressureSystolic) &&
-        (70.0..90.0).contains(bloodPressureDiastolic) &&
-        (70.0..110.0).contains(pulse))
-    {
-        // sad
-        println("\uD83D\uDE25")
-    }
-
-    else if ((1.0..3.5).contains(acceleration) &&
-        (120.0..160.0).contains(bloodPressureSystolic) &&
-        (80.0..100.0).contains(bloodPressureDiastolic) &&
-        (90.0..140.0).contains(pulse))
-    {
-        // stressed
-        println("\uD83D\uDE27")
-    }
-
-    else println("❓")
 }
